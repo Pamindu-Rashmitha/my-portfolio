74b6118d389d2d71d1ebfec74c9ff9a81e2709e8
IT24102819 <176378655+IT24102819@users.noreply.github.com>
2026-01-12 16:15:03 +0530

    Refactor code structure for improved readability and maintainability

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,6 +2,8 @@ import storeImg from '../assets/store.jpeg';
 import heartImg from '../assets/heart.png';
 import restImg from '../assets/restaurant.png';
 import pamiya from '../assets/pami-gpt.png';
+import vehicle from '../assets/vehicle.png';
+import portfolio from '../assets/portfolio.png';
 
 
 export interface Project {
@@ -50,4 +52,21 @@ export const projects: Project[] = [
         liveDemo:"https://pamiya-gpt-fzv4byvuzavwvv8na975u8.streamlit.app/",
         image: pamiya
     },
+    {
+        id:5,
+        title:"Web Based Vehicle Reservation System",
+        description:"This project is a full-stack, enterprise-grade Web-Based Vehicle Rental System developed using Spring Boot. It features a robust architecture integrating Stripe API for secure payments, Spring Security for role-based access control (Admin/User), and MySQL for data management. Key functionalities include real-time vehicle availability searching, automated email notifications, a customer support ticketing system, and a comprehensive administrative dashboard for fleet and revenue analytics.",
+        technologies:["Java", "Spring Boot", "Spring Security", "spring Data JPA/Hibernate", "Thymeleaf","MySQL","Stripe API","Gmail SMTP"],
+        githubLink:"https://github.com/Pamindu-Rashmitha/web-based-vehicle-rental",
+        image:vehicle
+    },
+    {
+        id:6,
+        title:"My Portfolio",
+        description:"A premium, interactive developer portfolio built with a focus on Artificial Intelligence and modern web technologies. This project showcases my journey as an AI student at SLIIT and my expertise in building intelligent, human-centered digital products.",
+        technologies:["React", "Type Script", "Tailwind CSS", "Framer Motion", "Lucide React"],
+        githubLink:"https://github.com/Pamindu-Rashmitha/my-portfolio",
+        image:portfolio
+    },
+
 ];

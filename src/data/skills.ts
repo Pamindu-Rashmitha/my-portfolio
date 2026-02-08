56e880be9861cbe22e9e01d30a4d133c5c77070f
Pamindu-Rashmitha <pamidurashmitha@gmail.com>
2026-02-08 13:06:55 +0530

    Add FastAPI to Backend skills for enhanced technology representation

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -2,9 +2,9 @@ import { Code2,Brain,Database,Layout,Smartphone,Terminal} from "lucide-react"
 
 export const skills = [
   { name: "Frontend", icon: Layout, items: ["React", "TypeScript", "Tailwind","HTML","CSS"]},
-  { name: "Backend", icon: Code2, items: ["SpringBoot"]},
+  { name: "Backend", icon: Code2, items: ["SpringBoot","FastAPI"]},
   { name: "Mobile", icon: Smartphone, items: ["React Native", "Expo"]},
-  { name: "Languages", icon: Terminal, items: ["JavaScript", "Python", "Java"]},
+  { name: "Languages", icon: Terminal, items: ["JavaScript", "Python", "Java","C"]},
   { name: "Databases", icon: Database, items: ["MySQL","Postgresql"]},
   { name: "AI/ML", icon: Brain, items:["Pandas","PyTorch","TensorFlow","LangChain","Numpy","Sci-kit Learn","Matplotlib","Seaborn"]}
 ]

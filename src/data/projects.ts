5985f30ff3c139874459e21b68a584b484f444c2
Pamindu-Rashmitha <pamidurashmitha@gmail.com>
2026-05-25 23:27:59 +0530

    Updated RentGo image

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,7 @@ import pamiya from '../assets/pami-gpt.png';
 import vehicle from '../assets/vehicle.png';
 import portfolio from '../assets/portfolio.png';
 import heritage from '../assets/Heritage-Slabs.jpeg';
-import RentGo from '../assets/RentGo.jpg';
+import RentGo from '../assets/RentGo.jpeg';
 
 
 export interface Project {
